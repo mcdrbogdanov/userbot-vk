ac302d254501e24d998f855738a1a135669ffe1f
mcdrbogdanov <106861562+mcdrbogdanov@users.noreply.github.com>
2023-01-04 07:06:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-access_token = ""  # токен пользователя (vk.cc/a6dCgm)
+access_token = "https://api.vk.com/blank.html#access_token=vk1.a.E1--kiqC6GYJrH04rqBsY0ZVWp4eoi1Q2rY3A_WqWiMYgEZB0HbRneltrpn4pxAiXq10vUNbBm3jNZolcCrQBy6gyFHGxZ_RZoVKeSMgy-u6EIxpC8Zp2Gc_K-Yk55MUY3_8N0TcsKQaMPCnthOTEj2j-FX58zg1fSUTl6QOMa8nPVUMvblQlgtcyHumyV3mf96UpnqOrxkQKrFUkD0NzA&expires_in=0&user_id=677016482"  # токен пользователя (vk.cc/a6dCgm)
 log_messages = True  # логировать ли сообщения
 
 prefixes = {  # префиксы, перед сообщениями
